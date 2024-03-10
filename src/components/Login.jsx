393a5d09623de46e654a6939717d682aea6c1925
Martin Roldan <ncr.mr250253@gmail.com>
2024-03-10 18:34:39 -0300

    comienzo de las validaciones

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,51 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 import '../style/login.css'
+import { useState } from 'react';
 
 function LoginFunction() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e)=>{
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const handleChangePassword = (e)=> {
+    setPassword(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const handleSubmit = (e)=>{
+    //prever que se refresque la pagina
+    e.preventDefault();
+    console.log('Submit event triggered');
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    //validaciones
+    if (!email.trim || !password.trim) {
+      alert('Por favor, complete todos los campos.');
+      return;
+    }
+  }
+
+
+
   return (
     <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
     <div className='login rounded'>
         <h2 className=' text-center mb-3'>Inicio</h2>
-        <form className='needs-validation'>
+        <form className='needs-validation' onSubmit={handleSubmit}>
         <div className='form-group was-validated mb-2'>
             <label htmlFor="email" className='form-label'>Ingrese su e-mail</label>
-            <input type="email" className='form-control' required ></input>
+            <input type="email" className='form-control' onChange={handleChangeEmail} value={email} required ></input>
             <div className='invalid-feedback'>
               Por favor ingrese su e-mail
             </div>
         </div>
         <div className='form-group was-validated mb-2'>
             <label htmlFor="password" className='form-label'>Contraseña</label>
-            <input type="password" className='form-control' required ></input>
+            <input type="password" className='form-control' onChange={handleChangePassword} required ></input>
             <div className='invalid-feedback'>
               Por favor ingrese Contraseña
             </div>
