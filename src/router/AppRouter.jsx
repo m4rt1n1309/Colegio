8ac3601f674a92d47a59730afd29c33a04d4d341
lucas ibañez <lk5bull@gmail.com>
2024-03-25 16:38:34 -0300

    registro incompleto

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LoginScreen } from "../components/LoginScreen";
 import { Administracion } from "../components/Administracion";
+import { RegistroScreen } from "../components/RegistroScreen";
 
 export const AppRouter = () => {
   const isLoggedIn = localStorage.getItem('token');
@@ -9,6 +10,8 @@ export const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginScreen />} />
+        <Route path="/registro" element={<RegistroScreen />} />
+
         <Route
           path="/administracion"
           element={isLoggedIn ? <Administracion /> : <Navigate to="/" />}
