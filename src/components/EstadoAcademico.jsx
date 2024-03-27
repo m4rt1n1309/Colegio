be455a413e400a71cff1a9d8997440535a43ec77
Martin Roldan <ncr.mr250253@gmail.com>
2024-03-27 20:05:36 -0300

    ultimo

--- src/components/EstadoAcademico.jsx
+++ src/components/EstadoAcademico.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import pruebaApi from "../api/pruebaApi";
 import { Table } from "react-bootstrap";
-import CargarNotas from "./cargarNotas";
 
 
 const EstadoAcademico = () => {
