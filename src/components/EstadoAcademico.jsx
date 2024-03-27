6647af9bdd50021a601727cfb251dffca22c7772
Bruno Tripi <brunoweh@hotmail.com>
2024-03-27 02:36:48 -0300

    mostras mas terminado

--- src/components/EstadoAcademico.jsx
+++ src/components/EstadoAcademico.jsx
@@ -1,71 +1,102 @@
-import { useEffect, useState } from 'react';
-import pruebaApi from '../api/pruebaApi';
-import { Table } from 'react-bootstrap';
+import { useEffect, useState } from "react";
+import pruebaApi from "../api/pruebaApi";
+import { Table } from "react-bootstrap";
 
 const EstadoAcademico = () => {
-    const [alumno, setAlumno] = useState(null);
-    const alumnoIdLocalStorage = localStorage.getItem('alumnoId'); // Obtener el ID del alumno del localStorage
+  const [alumno, setAlumno] = useState(null);
+  const alumnoIdLocalStorage = localStorage.getItem("alumnoId"); // Obtener el ID del alumno del localStorage
 
-    useEffect(() => {
-        const obtenerAlumno = async () => {
-            try {
-                if (!alumnoIdLocalStorage) {
-                    console.error('No se encontró el ID del alumno en el localStorage');
-                    return;
-                }
-                // Realizar la solicitud al backend para obtener la lista de alumnos
-                const resp = await pruebaApi.get("/admin/listaalumnos/");
-                // Obtener la lista de alumnos del backend
-                const listaAlumnos = resp.data.listaAlumnos;
-                
-                // Buscar el alumno con el ID que coincida con el ID del localStorage
-                const alumnoEncontrado = listaAlumnos.find(alumno => alumno._id === alumnoIdLocalStorage);
-                
-                if (alumnoEncontrado) {
-                    // Si se encuentra el alumno, establecer el estado del alumno con los datos encontrados
-                    setAlumno(alumnoEncontrado);
-                } else {
-                    console.error('No se encontró el alumno con el ID especificado');
-                }
-            } catch (error) {
-                console.error('Error al obtener los datos del alumno:', error);
-            }
-        };
+  useEffect(() => {
+    const obtenerAlumno = async () => {
+      try {
+        if (!alumnoIdLocalStorage) {
+          console.error("No se encontró el ID del alumno en el localStorage");
+          return;
+        }
+        // Realizar la solicitud al backend para obtener la lista de alumnos
+        const resp = await pruebaApi.get("/admin/listaalumnos/");
+        // Obtener la lista de alumnos del backend
+        const listaAlumnos = resp.data.listaAlumnos;
 
-        obtenerAlumno();
-    }, [alumnoIdLocalStorage]); // Se ejecutará cada vez que cambie el ID del alumno en el localStorage
+        // Buscar el alumno con el ID que coincida con el ID del localStorage
+        const alumnoEncontrado = listaAlumnos.find(
+          (alumno) => alumno._id === alumnoIdLocalStorage
+        );
 
-    if (!alumno) {
-        return <p>Cargando...</p>; // Muestra un mensaje de carga mientras se obtienen los datos del alumno
-    }
+        if (alumnoEncontrado) {
+          // Si se encuentra el alumno, establecer el estado del alumno con los datos encontrados
+          setAlumno(alumnoEncontrado);
+        } else {
+          console.error("No se encontró el alumno con el ID especificado");
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos del alumno:", error);
+      }
+    };
 
-    return (
-        <>
-            <h1 className='textoAlumnos'> Estado Académico del Alumno</h1>
-        
-            <Table striped bordered hover variant="ligth" style={{ borderCollapse: 'collapse', border: '2px solid black' }}>
-                <thead className='bold'>
-                    <tr>
-                        <th>Materias</th>
-                        <th>Nota</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* Renderizar las materias y notas del alumno */}
-                    {/* Asegúrate de ajustar esto según la estructura de datos en tu backend */}
-                    <tr>
-                        <td>Matemáticas</td>
-                        <td>{alumno.matematicas}</td>
-                    </tr>
-                    <tr>
-                        <td>Lengua y Literatura</td>
-                        <td>{alumno.lenguaLiteratura}</td>
-                    </tr>
-                    {/* Agrega más filas para las otras materias */}
-                </tbody>
-            </Table>
-        </>
-    );
+    obtenerAlumno();
+  }, [alumnoIdLocalStorage]); // Se ejecutará cada vez que cambie el ID del alumno en el localStorage
+
+  if (!alumno) {
+    return <p>Cargando...</p>; // Muestra un mensaje de carga mientras se obtienen los datos del alumno
+  }
+  console.log(alumno);
+  return (
+    <>
+      <h1 className="textoAlumnos"> Estado Académico del Alumno</h1>
+
+      <Table
+        striped
+        bordered
+        hover
+        variant="ligth"
+        style={{ borderCollapse: "collapse", border: "2px solid black" }}
+      >
+        <thead className="bold">
+          <tr>
+            <th>Materias</th>
+            <th>Nota</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Renderizar las materias y notas del alumno */}
+          {/* Asegúrate de ajustar esto según la estructura de datos en tu backend */}
+          <tr>
+            <td>Matemáticas</td>
+            <td>{alumno.matematicas}</td>
+          </tr>
+          <tr>
+            <td>Lengua y Literatura</td>
+            <td>{alumno.lenguaLiteratura}</td>
+          </tr>
+          <tr>
+            <td>Biologia</td>
+            <td>{alumno.biologia}</td>
+          </tr>
+          <tr>
+            <td>Física Química</td>
+            <td>{alumno.fisica}</td>
+          </tr>
+          <tr>
+            <td>Economía</td>
+            <td>{alumno.economia}</td>
+          </tr>
+          <tr>
+            <td>Geografía</td>
+            <td>{alumno.geografia}</td>
+          </tr>
+          <tr>
+            <td>Historia</td>
+            <td>{alumno.historia}</td>
+          </tr>
+          <tr>
+            <td>Educación Física</td>
+            <td>{alumno.educacionFisica}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
 
 export default EstadoAcademico;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,17 +3,17 @@ import { LoginScreen } from "../components/LoginScreen";
 import { Administracion } from "../components/Administracion";
 import { RegistroScreen } from "../components/RegistroScreen";
 import { Alumnos } from "../components/Alumnos";
-import { EstadoAcademico } from "../components/EstadoAcademico";
+import EstadoAcademico from "../components/EstadoAcademico";
 
 export const AppRouter = () => {
-  const isLoggedIn = localStorage.getItem('token');
+  const isLoggedIn = localStorage.getItem("token");
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginScreen />} />
         <Route path="/registro" element={<RegistroScreen />} />
-        <Route path="/alumnos/:id" element={<EstadoAcademico/>} />
+        <Route path="/alumnos/:id" element={<EstadoAcademico />} />
         <Route
           path="/alumnos"
           element={isLoggedIn ? <Alumnos /> : <Navigate to="/" />}
