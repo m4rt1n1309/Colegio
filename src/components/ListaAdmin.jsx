114197de4004e4abd83a116827e089cfa4b578ab
Martin Roldan <ncr.mr250253@gmail.com>
2024-03-28 23:26:04 -0300

    funcion eliminar admin

--- src/components/ListaAdmin.jsx
+++ src/components/ListaAdmin.jsx
@@ -3,14 +3,15 @@ import pruebaApi from "../api/pruebaApi";
 import { useState } from "react";
 import { Table } from "react-bootstrap";
 import "../style/login.css";
+import Swal from "sweetalert2";
 
 export const ListaAdmin = () => {
   const [cargarAdmin, setCargarAdmin] = useState([]);
   const ListaAdmin = async () => {
     try {
-      const resp = await pruebaApi.get("/admin/listaadmin");
+      const resp = await pruebaApi.get("/admin/listaadmins");
       setCargarAdmin(resp.data.listaAdmin);
-      console.log(cargarAdmin);
+      
     } catch (error) {
       console.log(error);
     }
@@ -19,6 +20,66 @@ export const ListaAdmin = () => {
   useEffect(() => {
     ListaAdmin();
   }, [cargarAdmin]);
+
+  const handleEliminarClick = async (id) => {
+    // Primero, verifica si el ID del administrador que se está intentando eliminar es el ID del superadmin
+    if (id === "65ff7cde8fcef532a189ff7d") {
+      // Si es el superadmin, muestra un mensaje de advertencia indicando que no se puede eliminar
+      Swal.fire({
+        icon: "warning",
+        title: "No se puede eliminar al superadmin",
+        text: "El superadmin no puede ser eliminado",
+      });
+      return; // Sale de la función sin continuar con la eliminación
+    }
+  
+    // Si no es el superadmin, muestra una ventana emergente de confirmación para eliminar el administrador
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Esta acción no se puede deshacer",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      // Si el usuario confirma la eliminación
+      if (result.isConfirmed) {
+        try {
+          // Realiza la solicitud para eliminar el alumno con el ID especificado
+          const response = await pruebaApi.delete(
+            `/admin/eliminaradmin/${id}`
+          );
+
+          // Verifica si la solicitud se completó con éxito (código de estado 200)
+          if (response.status === 200) {
+            // Muestra un mensaje de éxito utilizando SweetAlert u otra biblioteca de notificación
+            Swal.fire({
+              icon: "success",
+              title: "Administrador eliminado correctamente",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+
+            // Aquí puedes realizar cualquier otra acción necesaria, como recargar la lista de alumnos
+          } else {
+            // Si la solicitud no se completó con éxito, muestra un mensaje de error
+            throw new Error("No se pudo eliminar el alumno");
+          }
+        } catch (error) {
+          // Captura y maneja cualquier error que pueda ocurrir durante el proceso
+          console.error("Error al eliminar el administrador:", error);
+          // Muestra un mensaje de error utilizando SweetAlert u otra biblioteca de notificación
+          Swal.fire({
+            icon: "error",
+            title: "Error al eliminar el administrador",
+            text: "Por favor, inténtalo de nuevo más tarde",
+          });
+        }
+      }
+    });
+  };
 
   return (
     <>
@@ -53,12 +114,12 @@ export const ListaAdmin = () => {
                 <td>{admin.telefono}</td>                
                 <td>
                   {/* Botón para eliminar */}
-                  {/* <button
+                  <button
                     style={{ backgroundColor: "red", color: "black" }}
-                    onClick={() => handleEliminarClick(alumno._id)}
+                    onClick={() => handleEliminarClick(admin._id)}
                   >
                     &#10060; 
-                  </button> */}
+                  </button>
                   
                   
                 </td>
