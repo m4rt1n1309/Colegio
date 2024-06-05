f2c7c724b015e6dd23fc5831337e0ea50560207d
Martin Roldan <ncr.mr250253@gmail.com>
2024-06-05 20:43:57 -0300

    nueva base de datos

--- src/api/pruebaApi.js
+++ src/api/pruebaApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pruebaApi = axios.create({
-    baseURL : "http://localhost:10000",
+    baseURL : "https://deploy-render-ka05.onrender.com",
 });
 
 pruebaApi.interceptors.request.use((config) =>{
